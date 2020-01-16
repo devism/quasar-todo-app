d72d7b8a03df7098dc1bcee6c4f2290c37a8beb6
oxvac <oxvac@bitbucket.org>
2020-01-16 15:14:44 -0800

    first commit

---
+++ src/pages/PageTodo.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-page class="">
+  <div class="q-pa-md" style="max-width: 350px">
+    <q-list bordered>
+
+
+      <q-item
+       v-for="task in tasks"
+       :key="task.id"
+       @click="task.completed = !task.completed"
+       clickable
+       tag="label" 
+       v-ripple>
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{task.name}}</q-item-label>
+        </q-item-section>
+      </q-item>
+
+ 
+
+    </q-list>
+  </div>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      tasks:[
+        {
+          id:1,
+          name: 'go to shop',
+          completed:false
+        },
+        {
+          id:2,
+          name: 'Get Bananas',
+          completed:false
+        },
+        {
+          id:3,
+          name: 'get apples',
+          completed:false
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/pages/Settings.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+    <p>settings</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
